fa7a48ca202021285561d048b004447a7868f36b
Lidiane Taquehara <lidi.mayra@gmail.com>
2017-09-13 03:24:21 +0000

    Exibe no LCD o status de um pedido raspado do site dos correios

---
+++ script.py
@@ -0,0 +1,20 @@
+from upm import pyupm_jhd1313m1 as lcd
+from bs4 import BeautifulSoup
+import requests
+import time
+
+url = 'http://www2.correios.com.br/sistemas/rastreamento/resultado.cfm'
+
+params = { 'objetos': 'codigo-de-rastreamento' }
+headers = { 'Referer': 'http://www2.correios.com.br/sistemas/rastreamento/default.cfm' }
+
+html = requests.post(url, data=params, headers=headers)
+
+while True:
+    soup = BeautifulSoup(html.text, 'html.parser')
+    text = soup.strong.text.encode('utf-8')
+
+    myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+    myLcd.write(text)
+    time.sleep(2)
+    myLcd.clear()
