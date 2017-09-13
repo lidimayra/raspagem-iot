650caaf7015e03e2b0b197f0e70e07941fec323a
Murilo VIviani <muriloviviani@hotmail.com>
2017-09-13 10:43:51 -0300

    Update script.py

--- script.py
+++ script.py
@@ -8,13 +8,14 @@ url = 'http://www2.correios.com.br/sistemas/rastreamento/resultado.cfm'
 params = { 'objetos': 'codigo-de-rastreamento' }
 headers = { 'Referer': 'http://www2.correios.com.br/sistemas/rastreamento/default.cfm' }
 
-html = requests.post(url, data=params, headers=headers)
-
-soup = BeautifulSoup(html.text, 'html.parser')
-text = soup.strong.text.encode('utf-8')
-
-myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
-myLcd.write(text)
-
 while True:
-    time.sleep(100)
+    html = requests.post(url, data=params, headers=headers)
+
+    soup = BeautifulSoup(html.text, 'html.parser')
+    text = soup.strong.text.encode('utf-8')
+
+    myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+    myLcd.write(text)
+
+    time.sleep(2)
+    
