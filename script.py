b91e73bf4f5d694bf69e0eedbb301ba4bdeceaac
Lidiane Taquehara <lidi.mayra@gmail.com>
2017-09-17 04:01:10 +0000

    Exibe diferentes cores dependendo do estado do pedido
    Se o pedido já tiver sido entregue, o LCD acenderá em verde, caso
    contrário, acenderá em azul

--- script.py
+++ script.py
@@ -4,19 +4,32 @@ import requests
 import sys
 import time
 
-url = 'http://www2.correios.com.br/sistemas/rastreamento/resultado.cfm'
+myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
 
-params = { 'objetos': sys.argv[1] }
-headers = { 'Referer': 'http://www2.correios.com.br/sistemas/rastreamento/default.cfm' }
-
-while True:
+def order_status():
+    url = 'http://www2.correios.com.br/sistemas/rastreamento/resultado.cfm'
+    params = { 'objetos': sys.argv[1] }
+    headers = { 'Referer': 'http://www2.correios.com.br/sistemas/rastreamento/default.cfm' }
     html = requests.post(url, data=params, headers=headers)
-
     soup = BeautifulSoup(html.text, 'html.parser')
-    text = soup.strong.text.encode('utf-8')
+    return soup.strong.text.encode('utf-8')
 
-    myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
-    myLcd.write(text)
+def lcd_color(green, blue):
+    red = 10
+    myLcd.setColor(red, green, blue)
+
+def lcd_write(word, line):
+    myLcd.setCursor(line, 0)
+    myLcd.write(word)
+
+def run():
+    words = order_status().split()
+    lcd_color(70, 30) if words[1] == 'entregue' else lcd_color(30, 70)
+
+    lcd_write(words[0], 0)
+    lcd_write(words[1], 1)
 
     time.sleep(2)
-    
+
+while True:
+    run() 
